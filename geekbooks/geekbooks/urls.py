81ad5a0b58f6f8e279082c5ba4cf24d5c48ba8ef
tsnv76 <tsnv76@yandex.ru>
2023-09-21 15:39:39 +0700

    commit new design of page

--- geekbooks/geekbooks/urls.py
+++ geekbooks/geekbooks/urls.py
@@ -20,8 +20,8 @@ from django.urls import path
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.main),
-    path('products/', mainapp.products),
-    path('contact/', mainapp.contact),
+    path('', mainapp.main, name='main'),
+    path('products/', mainapp.products, name='products'),
+    path('contact/', mainapp.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
